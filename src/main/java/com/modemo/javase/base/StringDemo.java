2ca741a5339b1764eef6692d3a867b56f692cf20
moshengwei <moshengwei@krspace.cn>
2017-05-09 10:59:58 +0800

    测试按字符串分割。

--- src/main/java/com/modemo/javase/base/StringDemo.java
+++ src/main/java/com/modemo/javase/base/StringDemo.java
@@ -2,6 +2,10 @@ package com.modemo.javase.base;
 
 public class StringDemo {
 	public static void main(String[] args) {
-		System.out.println("1".equals(null));
+//		System.out.println("1".equals(null));
+		String[] ids = "1".split(",");
+		for (String id : ids) {
+			System.out.println(id);
+		}
 	}
 }
