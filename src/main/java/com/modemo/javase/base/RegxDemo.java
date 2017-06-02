1f2eba790fd716872eb2895743ed2a208a50c985
moshengwei <moshengwei@krspace.cn>
2017-06-02 13:32:06 +0800

    1.添加正则 find 方法测试。 2.添加编号连续逻辑。

--- src/main/java/com/modemo/javase/base/RegxDemo.java
+++ src/main/java/com/modemo/javase/base/RegxDemo.java
@@ -5,14 +5,17 @@ import java.util.regex.Pattern;
 
 public class RegxDemo {
 	public static void main(String[] args) {
-		Pattern p = Pattern.compile("[a-zA-Z]{2}");
-		String t = "aabbcc";
+		Pattern p = Pattern.compile("^[a-zA-Z]+");
+		String t = "01s23456";
 		Matcher m = p.matcher(t);
 //		while (m.find()) {
 //			System.out.println("s = " + m.start() + ",e = " + m.end());
 //		}
-		m.find();
-		System.out.println("s = " + m.start() + ",e = " + m.end());
-		System.out.println(t.substring(m.end()));
+		if(m.find()){
+			System.out.println("s = " + m.start() + ",e = " + m.end());
+			System.out.println(t.substring(m.end()));
+		} else {
+			System.out.println(t);
+		}
 	}
 }
--- src/main/java/com/modemo/javase/base/StringDemo.java
+++ src/main/java/com/modemo/javase/base/StringDemo.java
@@ -1,11 +1,58 @@
 package com.modemo.javase.base;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringDemo {
+	private static Pattern prefixRegx = Pattern.compile("^[a-zA-Z]+");// 判断工位编号连续前缀
+
 	public static void main(String[] args) {
-//		System.out.println("1".equals(null));
-		String[] ids = "1".split(",");
-		for (String id : ids) {
-			System.out.println(id);
+		// System.out.println("1".equals(null));
+		// String[] ids = "1".split(",");
+		// for (String id : ids) {
+		// System.out.println(id);
+		// }
+//		String testSubString = "aabbcc";
+//		testSubString = testSubString.substring(0);
+//		System.out.println(testSubString);
+		boolean isContinue = isContinue("1001", "1002");
+		System.out.println(isContinue);
+	}
+
+	private static boolean isContinue(String a, String b) {
+		boolean isContinue = false;
+		String aSuffix;
+		String bSuffix;
+		// 1.判断前缀是否一致
+		Matcher am = prefixRegx.matcher(a);
+		Matcher bm = prefixRegx.matcher(b);
+		boolean aIsLetter = am.find();
+		boolean bIsLetter = bm.find();
+		if (aIsLetter == bIsLetter) {
+			aSuffix = a;
+			bSuffix = b;
+			// 2.判断是否以字母开头
+			if (true == aIsLetter) {
+				// 3.字母部分是否一致
+				String aPrefix = a.substring(am.start(), am.end());
+				String bPrefix = b.substring(bm.start(), bm.end());
+				if (aPrefix.equals(bPrefix)) {
+					// 4.获取纯数字部分
+					aSuffix = a.substring(am.end());
+					bSuffix = b.substring(bm.end());
+				}
+			}
+			// 5.数字部分是否连续
+			try {
+				int aInt = Integer.valueOf(aSuffix);
+				int bInt = Integer.valueOf(bSuffix);
+
+				if (Math.abs(bInt - aInt) == 1) {
+					isContinue = true;
+				}
+			} catch (Exception e) {
+			}
 		}
+		return isContinue;
 	}
 }
