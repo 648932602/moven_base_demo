c55f4f1fc8b1dc29239cc17d6c7a3ce3c7e82262
moshengwei <moshengwei@krspace.cn>
2018-05-17 11:48:55 +0800

    生成pkcs12证书。

---
+++ src/main/java/com/modemo/javase/util/Extension.java
@@ -0,0 +1,22 @@
+package com.modemo.javase.util;
+
+public class Extension {
+
+    private String oid;
+
+    private boolean critical;
+
+    private byte[] value;
+
+    public String getOid() {
+        return oid;
+    }
+
+    public byte[] getValue() {
+        return value;
+    }
+
+    public boolean isCritical() {
+        return critical;
+    }
+}
